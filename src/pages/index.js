3cd0fb571b114a947ef87e821bc22fe78f332070
teygeta <vitto.gioda@outlook.it>
2023-03-17 13:05:55 +0100

    installed framer-motion

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,11 @@
 import Image from "next/image"
+import { motion, useScroll, useTransform } from "framer-motion"
 
 export default function Home() {
+
+  let { scrollY } = useScroll()
+  let y = useTransform(scrollY, [0, 1500], ["0%", "15%"])
+
   return (
     <>
       <nav className="bg-[#F6E4D6] fixed top-0 w-full z-20">
@@ -28,7 +33,13 @@ export default function Home() {
       <div className="h-[112px]"></div>
       <header className="bg-[#F6E4D6]">
         <div className="flex justify-around max-w-[1400px] mx-auto">
-          <div className="my-20">
+          <div
+            transition={{
+              duration: 1,
+            }}
+            initial={{ translateX: "-100%" }}
+            whileInView={{ translateX: 0 }}
+            className="my-20">
             <h1 className="font-bold text-4xl">Ti aiutiamo a scegliere <br />
               le apparecchiature <br />
               e gli accessori piu’ idonei <br />
@@ -62,16 +73,17 @@ export default function Home() {
         <p className="max-w-xl text-xl mt-20 mx-8">
           Grazie alla nostra vasta esperienza e alla disponibilità di prodotti di alta qualità, Beauty Global S.R.L. è in grado di offrire soluzioni personalizzate per la realizzazione di centri abbronzatura, centri estetici ed istituti di bellezza.
           <br /><br />
-          Oltre alla fornitura di attrezzature per l'estetica professionale e di lampade abbronzanti, offriamo anche piani d'investimento su misura, assistenza tecnica e commerciale e supporto di professionisti specializzati nella realizzazione dei centri.
+          Oltre alla fornitura di attrezzature per l&apos;estetica professionale e di lampade abbronzanti, offriamo anche piani d&apos;investimento su misura, assistenza tecnica e commerciale e supporto di professionisti specializzati nella realizzazione dei centri.
         </p>
-        <Image
-          src="/pexels-polina-tankilevitch-3736520.png"
-          alt="shop"
-          width={570}
-          height={40}
-          className="rounded mr-8 z-10 shadow-[0_0_36px_rgba(0,0,0,0.20)]"
-        />
-
+        <motion.div style={{ y }}>
+          <Image
+            src="/pexels-polina-tankilevitch-3736520.png"
+            alt="shop"
+            width={570}
+            height={40}
+            className="rounded mr-8 z-10 shadow-[0_0_36px_rgba(0,0,0,0.20)]"
+          />
+        </motion.div>
       </section>
       <section className="bg-[#F6E4D6] mt-[-230px] py-20">
         <div className="flex justify-between items-end mx-auto max-w-[1400px]">
@@ -87,16 +99,17 @@ export default function Home() {
             <br /><br />
             Ogni apparecchiatura estetica viene testata e certificata per garantire il massimo risultato, minimizzando i rischi per la salute dei clienti.
             <br /><br />
-            Inoltre, collaboriamo con importanti società finanziarie per offrire piani d'investimento flessibili e convenienti, adattabili alle esigenze di ogni cliente.
+            Inoltre, collaboriamo con importanti società finanziarie per offrire piani d&apos;investimento flessibili e convenienti, adattabili alle esigenze di ogni cliente.
             <br /><br />
-            Scegliere Beauty Global S.R.L. significa affidarsi a un team di professionisti esperti, sempre pronti a fornire assistenza e consulenza, sia prima che dopo l'acquisto
+            Scegliere Beauty Global S.R.L. significa affidarsi a un team di professionisti esperti, sempre pronti a fornire assistenza e consulenza, sia prima che dopo l&apos;acquisto
           </p>
         </div>
       </section>
-      <footer className="border-t-[1px] border-[#8B8B8B] mt-20 mx-10">
+
+      <footer className="border-t-[1px] border-[#8B8B8B] mt-20 mx-10 text-sm">
         <div className="flex justify-around items-center max-w-[1200px] mx-auto p-5">
-          <div><p>Beauty Global s.r.l</p></div>
-          <div><p> p.iva 10860510014</p></div>
+          <div><p>Bea Glob s.r.l</p></div>
+          <div><p> p.iva 10220220022</p></div>
           <div className="absolute left-1/2 -translate-x-1/2">
             <Image
               src="/logo-white.svg"
